16460acbc206c6d0409d6e865b30abcd1fade571
kristina.gagalovska <kristinagagalovska@gmail.com>
2016-04-10 19:58:20 +0200

    Create AdminMiddleware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,53 +17,53 @@ Route::get('/', function () {
 
 Route::auth();
 
-Route::get('/public', 'HomeController@index');
+Route::get('/public', 'HomeController@index')->middleware('auth');
 
 //page for the users to edit their information
-Route::get('/user/edit/{id}', 'UserController@edit')->name('user.edit');
-Route::post('/user/edit/{id}', 'UserController@update')->name('user.edit');
+Route::get('/user/edit/{id}', 'UserController@edit')->name('user.edit')->middleware('auth');
+Route::post('/user/edit/{id}', 'UserController@update')->name('user.edit')->middleware('auth');
 
 //user can view all free pokemons
-Route::get('/user/pokemons/{id}', 'UserController@freePokemons')->name('user.pokemons');
+Route::get('/user/pokemons/{id}', 'UserController@freePokemons')->name('user.pokemons')->middleware('auth');
 
 //user can choose pokemon
-Route::get('/user/choose/{id}', 'UserController@choosePokemon')->name('user.choose');
-Route::post('/user/choose/{id}', 'UserController@savePokemon')->name('user.choose');
+Route::get('/user/choose/{id}', 'UserController@choosePokemon')->name('user.choose')->middleware('auth');
+Route::post('/user/choose/{id}', 'UserController@savePokemon')->name('user.choose')->middleware('auth');
 
 //user can view their pokemons
-Route::get('user/mypokemons/{id}', 'UserController@myPokemons')->name('user.mypokemons');
+Route::get('user/mypokemons/{id}', 'UserController@myPokemons')->name('user.mypokemons')->middleware('auth');
 
 //user can abandon pokemon
-Route::get('/user/abandon/{id}', 'UserController@abandon')->name('user.abandon');
-Route::post('/user/abandon/{id}', 'UserController@saveAbandon')->name('user.abandon');
+Route::get('/user/abandon/{id}', 'UserController@abandon')->name('user.abandon')->middleware('auth');
+Route::post('/user/abandon/{id}', 'UserController@saveAbandon')->name('user.abandon')->middleware('auth');
 
 //user can view total strength
-Route::get('/user/strength', 'UserController@strength')->name('user.strength');
+Route::get('/user/strength', 'UserController@strength')->name('user.strength')->middleware('auth');
 
 
 //admin panel
-Route::get('/admin', 'AdminController@admin')->name('admin');
+Route::get('/admin', 'AdminController@admin')->name('admin')->middleware('isAdmin');
 
 //admin can view all users
-Route::get('/admin/users/view', 'AdminController@view')->name('admin.users.view');
+Route::get('/admin/users/view', 'AdminController@view')->name('admin.users.view')->middleware('isAdmin');
 
 //admin can delete users
-Route::get('/admin/users/delete/{id}', 'AdminController@delete')->name('admin.users.delete');
+Route::get('/admin/users/delete/{id}', 'AdminController@delete')->name('admin.users.delete')->middleware('isAdmin');
 
 //admi can mark users as admin
-Route::get('/admin/users/mark/{id}', 'AdminController@mark')->name('admin.users.mark');
-Route::post('/admin/users/mark/{id}', 'AdminController@save')->name('admin.users.mark');
+Route::get('/admin/users/mark/{id}', 'AdminController@mark')->name('admin.users.mark')->middleware('isAdmin');
+Route::post('/admin/users/mark/{id}', 'AdminController@save')->name('admin.users.mark')->middleware('isAdmin');
 
 //admin can create new pokemon
-Route::get('/admin/pokemons/add', 'AdminController@create')->name('admin.pokemons.add');
-Route::post('/admin/pokemons/add', 'AdminController@store')->name('admin.pokemons.add');
+Route::get('/admin/pokemons/add', 'AdminController@create')->name('admin.pokemons.add')->middleware('isAdmin');
+Route::post('/admin/pokemons/add', 'AdminController@store')->name('admin.pokemons.add')->middleware('isAdmin');
 
 //admin can view all pokemons
-Route::get('/admin/pokemons/show', 'AdminController@show')->name('admin.pokemons.show');
+Route::get('/admin/pokemons/show', 'AdminController@show')->name('admin.pokemons.show')->middleware('isAdmin');
 
 //admin can edit pokemon
-Route::get('/admin/pokemons/edit/{id}', 'AdminController@edit')->name('admin.pokemons.edit');
-Route::post('/admin/pokemons/edit/{id}', 'AdminController@update')->name('admin.pokemons.edit');
+Route::get('/admin/pokemons/edit/{id}', 'AdminController@edit')->name('admin.pokemons.edit')->middleware('isAdmin');
+Route::post('/admin/pokemons/edit/{id}', 'AdminController@update')->name('admin.pokemons.edit')->middleware('isAdmin');
 
 //admin can delete pokemon
-Route::get('/admin/pokemons/delete/{id}', 'AdminController@deletePokemon')->name('admin.pokemons.delete');
+Route::get('/admin/pokemons/delete/{id}', 'AdminController@deletePokemon')->name('admin.pokemons.delete')->middleware('isAdmin');
